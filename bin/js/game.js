06f20a59f4d065c2a3fa6e56b94a9c416ada8857
BoozeMoose <dave.nussbaumer@gmail.com>
2017-05-31 11:56:28 +0200

    before iso

--- bin/js/game.js
+++ bin/js/game.js
@@ -3,51 +3,103 @@ var SimpleGame = (function () {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create, update: this.update });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image("sky", "assets/sky.png");
-        this.game.load.image("ground", "assets/platform.png");
-        this.game.load.image("star", "assets/star.png");
-        this.game.load.spritesheet("dude", "assets/dude.png", 32, 48);
+        this.game.load.image("tiles", "assets/basictiles.png");
     };
     SimpleGame.prototype.create = function () {
+        //  Create some map data dynamically
+        //  Map size is 128x128 tiles
+        var dataLayer1 = "";
+        var dataLayer2 = "";
+        for (var y = 0; y < 64; y++) {
+            for (var x = 0; x < 64; x++) {
+                // data += this.game.rnd.between(0, 20).toString();
+                dataLayer1 += "11";
+                if (x < 65) {
+                    dataLayer1 += ",";
+                }
+            }
+            if (y < 65) {
+                dataLayer1 += "\n";
+            }
+        }
+        for (var y = 0; y < 64; y++) {
+            for (var x = 0; x < 64; x++) {
+                var temp = this.game.rnd.between(0, 20);
+                // data += this.game.rnd.between(0, 20).toString();
+                if (temp === 0) {
+                    dataLayer2 += "12";
+                }
+                else {
+                    dataLayer2 += "11";
+                }
+                if (x < 65) {
+                    dataLayer2 += ",";
+                }
+            }
+            if (y < 65) {
+                dataLayer2 += "\n";
+            }
+        }
+        //  Add data to the cache
+        // this.game.cache.addTilemap("dynamicMap", null, dataLayer1, Phaser.Tilemap.CSV);
+        this.game.load.tilemap("ground", null, dataLayer2, Phaser.Tilemap.CSV);
+        // this.game.load.tilemap("grass", null, dataLayer2, Phaser.Tilemap.CSV);
+        //  Create our map (the 16x16 is the tile size)
+        this.map = this.game.add.tilemap("ground", 16, 16);
+        //  'tiles' = cache image key, 16x16 = tile size
+        this.map.addTilesetImage("tiles", "tiles", 16, 16);
+        //  0 is important
+        this.map.createLayer(0).resizeWorld();
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.add.sprite(0, 0, "sky");
-        this.platforms = this.game.add.group();
-        this.platforms.enableBody = true;
-        var ground = this.platforms.create(0, this.game.world.height - 64, "ground");
-        ground.scale.setTo(2, 2);
-        ground.body.immovable = true;
-        var ledge = this.platforms.create(400, 400, "ground");
-        ledge.body.immovable = true;
-        ledge = this.platforms.create(-150, 250, "ground");
-        ledge.body.immovable = true;
-        // this.game.add.sprite(0, 0, "star");
-        this.player = this.game.add.sprite(32, this.game.world.height - 150, "dude");
-        this.game.physics.arcade.enable(this.player);
-        this.player.body.bounce.y = 0.2;
-        this.player.body.gravity.y = 300;
-        this.player.body.collideWorldBounds = true;
-        this.player.animations.add("left", [0, 1, 2, 3], 10, true);
-        this.player.animations.add("right", [5, 6, 7, 8], 10, true);
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        // this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        // this.game.add.sprite(0, 0, "sky");
+        // this.platforms = this.game.add.group();
+        // this.platforms.enableBody = true;
+        // const ground = this.platforms.create(0, this.game.world.height - 64, "ground");
+        // ground.scale.setTo(2, 2);
+        // ground.body.immovable = true;
+        // let ledge = this.platforms.create(400, 400, "ground");
+        // ledge.body.immovable = true;
+        // ledge = this.platforms.create(-150, 250, "ground");
+        // ledge.body.immovable = true;
+        // this.player = this.game.add.sprite(32, this.game.world.height - 150, "dude");
+        // this.game.physics.arcade.enable(this.player);
+        // this.player.body.bounce.y = 0.2;
+        // this.player.body.gravity.y = 300;
+        // this.player.body.collideWorldBounds = true;
+        // this.player.animations.add("left", [0, 1, 2, 3], 10, true);
+        // this.player.animations.add("right", [5, 6, 7, 8], 10, true);
+        // this.cursors = this.game.input.keyboard.createCursorKeys();
     };
     SimpleGame.prototype.update = function () {
-        var hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
-        this.player.body.velocity.x = 0;
+        // const hitPlatform: boolean = this.game.physics.arcade.collide(this.player, this.platforms);
         if (this.cursors.left.isDown) {
-            this.player.body.velocity.x = -150;
-            this.player.animations.play("left");
+            this.game.camera.x--;
         }
         else if (this.cursors.right.isDown) {
-            this.player.body.velocity.x = 150;
-            this.player.animations.play("right");
+            this.game.camera.x++;
         }
-        else {
-            this.player.animations.stop();
-            this.player.frame = 4;
+        if (this.cursors.up.isDown) {
+            this.game.camera.y--;
         }
-        if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform) {
-            this.player.body.velocity.y = -350;
+        else if (this.cursors.down.isDown) {
+            this.game.camera.y++;
         }
+        // this.player.body.velocity.x = 0;
+        // if (this.cursors.left.isDown) {
+        //     this.player.body.velocity.x = -150;
+        //     this.player.animations.play("left");
+        // } else if (this.cursors.right.isDown) {
+        //     this.player.body.velocity.x = 150;
+        //     this.player.animations.play("right");
+        // } else {
+        //     this.player.animations.stop();
+        //     this.player.frame = 4;
+        // }
+        // if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform) {
+        //     this.player.body.velocity.y = -350;
+        // }
     };
     return SimpleGame;
 }());
